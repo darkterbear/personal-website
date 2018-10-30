a9576fb723006736518de248fe04f9d758da098c
Terrance Li <terranceli.terbear@gmail.com>
2018-10-30 15:49:15 -0700

    Restructured code text

--- src/Components.js
+++ src/Components.js
@@ -11,3 +11,36 @@ export class BottomBar extends Component {
 		)
 	}
 }
+
+export class CodePane extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			current: 0,
+			age: ((Date.now() - 973152000000) / 1000 / 60 / 60 / 24 / 365).toFixed(8)
+		}
+	}
+
+	async componentDidMount() {
+		setInterval(() => {
+			this.setState({
+				age: ((Date.now() - 973152000000) / 1000 / 60 / 60 / 24 / 365).toFixed(
+					8
+				)
+			})
+		}, 100)
+	}
+
+	render() {
+		const homePageLines = [
+			<span>{'var terrance = new Developer({'}</span>,
+			<span>{'\n\u00A0\u00A0age: ' + this.state.age + ','}</span>,
+			<span>{"\n\u00A0\u00A0email: 'tyli@uw.edu',"}</span>,
+			<span>{"\n\u00A0\u00A0location: 'Seattle'"}</span>,
+			<span>{'\n})'}</span>
+		]
+
+		return <p className="code">{homePageLines}</p>
+	}
+}
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
-import { BottomBar } from './Components'
+import { BottomBar, CodePane } from './Components'
 
 const homeText =
 	'var terrance = new Developer({\n\u00A0\u00A0age: 17.99992,\n\u00A0\u00A0email: \u2018tyli@uw.edu\u2019,\n\u00A0\u00A0location: \u2018Seattle\u2019\n})\n\nterrance.addGitHub()\nterrance.addLinkedIn()\n\nterrance.resume = new Resume(\n\u00A0\u00A0terrance.experience,\n\u00A0\u00A0terrance.honors,\n\u00A0\u00A0terrance.projects\n)'
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
+}
+const replaceAt = (s, index, replacement) => {
+	return s.substr(0, index) + replacement + s.substr(index + replacement.length)
+}
+
+const hat = length => {
+	var text = ''
+	var possible = 'abcdef0123456789'
+
+	for (var i = 0; i < length; i++)
+		text += possible.charAt(Math.floor(Math.random() * possible.length))
+
+	return text
 }
 
 export default class HomePage extends Component {
@@ -14,22 +27,36 @@ export default class HomePage extends Component {
 
 		this.state = {
 			text: '',
-			nameText: 'terrance li'
+			nameText: hat(8) + ' ' + hat(2)
 		}
 	}
 
 	async componentDidMount() {
-		await sleep(1000)
+		//this.renderText()
+
+		this.revealName()
+	}
+
+	renderText = async () => {
+		await sleep(2000)
 		for (var i = 0; i < homeText.length; i++) {
 			await sleep(Math.random() * 40 + 30)
 			this.setState({ text: this.state.text + homeText.charAt(i) })
 		}
-
-		this.jitterNameText()
 	}
 
-	jitterNameText = async () => {
-		while (1) {
+	handlePageChange = pageName => {
+		console.log(pageName)
+	}
+
+	revealName = async () => {
+		await sleep(1000)
+
+		const actual = 'terrance li'
+		for (var i = 0; i < 11; i++) {
+			this.setState({
+				nameText: replaceAt(this.state.nameText, i, actual.charAt(i))
+			})
 			await sleep(50)
 		}
 	}
@@ -39,7 +66,7 @@ export default class HomePage extends Component {
 			<div className="content">
 				<div className="center">
 					<h1>{this.state.nameText}</h1>
-					<p className="code">{this.state.text}</p>
+					<CodePane handlePageChange={this.handlePageChange} />
 				</div>
 				<BottomBar />
 			</div>
