3ec1b997bbd7837bbfd14bf1ac49875b18a459e5
Terrance Li <terranceli.terbear@gmail.com>
2018-10-29 21:27:52 -0700

    Initialize NPM & React projects; add assets

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<Switch>{/* <Route component={} /> */}</Switch>
+			</Router>
+		)
+	}
+}
