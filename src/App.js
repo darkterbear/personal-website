d6a920164028346988db9ee140b88d1c7122eed5
Terrance Li <terranceli.terbear@gmail.com>
2019-05-08 17:03:23 -0700

    Simplified homepage

--- src/App.js
+++ src/App.js
@@ -7,29 +7,36 @@ export default class App extends React.Component {
         <div class='page'>
           <div class='top'>
             <h1>terrance li</h1>
-            <ul>
-              <li>experience</li>
-              <li>projects</li>
-              <li>resume</li>
-            </ul>
           </div>
           <div class='page-content'>
             <h2>Hey, I'm Terrance! I design and create web apps :)</h2>
           </div>
           <div class='footer'>
-            <h4>
-              <a
-                href='https://www.linkedin.com/in/terrance-li/'
-                target='_blank'
-              >
-                linkedin
-              </a>
-              : terrance-li{' '}
-              <a href='https://github.com/darkterbear' target='_blank'>
-                github
-              </a>
-              : darkterbear <a href='mailto:tyli@uw.edu'>email</a>: tyli@uw.edu
-            </h4>
+            <ul>
+              <li>
+                <a
+                  href='https://www.linkedin.com/in/terrance-li/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  linkedin
+                </a>
+                : terrance-li
+              </li>
+              <li>
+                <a
+                  href='https://github.com/darkterbear'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  github
+                </a>
+                : darkterbear
+              </li>
+              <li>
+                <a href='mailto:tyli@uw.edu'>email</a>: tyli@uw.edu
+              </li>
+            </ul>
           </div>
         </div>
       </div>
