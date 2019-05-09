e31eea7d35a99020d1b160c6d214a35ad6b214aa
Terrance Li <terranceli.terbear@gmail.com>
2019-05-08 17:52:10 -0700

    Use bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Card } from './Components'
 
 export default class App extends React.Component {
   render() {
     return (
       <div class='content'>
         <div class='page'>
-          <div class='top'>
-            <h1>terrance li</h1>
+          <div class='container'>
+            <div class='page-content'>
+              <h1>terrance li</h1>
+              <h2>Hey, I'm Terrance! I design and create web apps :)</h2>
+            </div>
           </div>
-          <div class='page-content'>
-            <h2>Hey, I'm Terrance! I design and create web apps :)</h2>
-          </div>
+
           <div class='footer'>
             <ul>
               <li>
@@ -40,12 +42,18 @@ export default class App extends React.Component {
           </div>
         </div>
         <div class='page'>
-          <h1>experience</h1>
-          <div class='container-fluid'>
+          <div class='container'>
+            <h1>experience</h1>
             <div class='row'>
-              <div class='col-sm'>test</div>
-              <div class='col-sm'>test</div>
-              <div class='col-sm'>test</div>
+              <div class='col-sm mb-3'>
+                <Card title={'test'}>lorem ipsum dolor sit amet</Card>
+              </div>
+              <div class='col-sm mb-3'>
+                <Card title={'test'}>lorem ipsum dolor sit amet</Card>
+              </div>
+              <div class='col-sm mb-3'>
+                <Card title={'test'}>lorem ipsum dolor sit amet</Card>
+              </div>
             </div>
           </div>
         </div>
--- src/Components.js
+++ src/Components.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react'
+
+export class Card extends Component {
+  render() {
+    return (
+      <div class='card'>
+        <h3>{this.props.title}</h3>
+        {this.props.children}
+      </div>
+    )
+  }
+}
